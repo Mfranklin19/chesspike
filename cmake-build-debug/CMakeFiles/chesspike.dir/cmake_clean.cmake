ec41f178643c9fe07fec69fa7871f62ac8266213
Michael Franklin <michaelfranklin208@outlook.com>
2021-09-18 18:09:37 -0500

    Created project that initializes chess board and prints textual representation

---
+++ cmake-build-debug/CMakeFiles/chesspike.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/chesspike.dir/main.cpp.o"
+  "chesspike"
+  "chesspike.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/chesspike.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
